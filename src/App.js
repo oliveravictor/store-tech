a47eb6fc0c815fb6a8124b88991bb14c99e1e1d9
Victor Olivera <ovictoriox@gmail.com>
2021-06-01 15:49:25 -0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World since React!</p>
-        <a
-          className="App-link"
-          href="https://es.reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Store Tech
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <header>
+      <label class="logo">Store Tech</label>
+      <ul>
+        <li>
+          <a href="#">Seccion#1</a>
+        </li>
+        <li>
+          <a href="#">Seccion#2</a>
+        </li>
+        <li>
+          <a href="#">Seccion#3</a>
+        </li>
+        <li>
+          <a href="#">Seccion#4</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Navbar;
