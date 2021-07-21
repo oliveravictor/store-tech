a8fa7d791b979cdc78f273e09eb4c66f06369c18
Victor Olivera <ovictoriox@gmail.com>
2021-07-21 17:56:21 -0300

    footer, credits and social media

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,8 +2,28 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <p className="footer">® Store Tech / 2021</p>
+    <div className={"footer"}>
+      <p className={"footer__title"}>® Store Tech / 2021</p>
+      <p className={"footer__credit"}>
+        Sitio hecho por
+        <a
+          className={"footer__autor"}
+          href="https://oliveravictor.github.io/siteup/"
+          target="_blank"
+        >
+          Víctor Olivera
+        </a>
+      </p>
+      <button className={"footer__button"}>
+        <a href="https://www.linkedin.com/in/victor-olivera/" target="_blank">
+          <i class="fab fa-linkedin-in"></i>
+        </a>
+      </button>
+      <button className={"footer__button"}>
+        <a href="https://github.com/oliveravictor" target="_blank">
+          <i class="fab fa-github"></i>
+        </a>
+      </button>
     </div>
   );
 };
